9b3a612ee87185e9aa15df1bc0577346d27117c9
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-23 21:55:41 +0900

    STUDY: zombie process.c

---
+++ Part01 네트워크 프로그래밍의 시작/Chapter10/zombie.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int gval = 10;
+int main(int argc, char *argv[])
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        puts("Hi, I am a child process");
+    }
+    else
+    {
+        printf("Child Process ID: %d \n", pid);
+        sleep(30);
+    }
+
+    if (pid == 0)
+        puts("End child process");
+    else
+        puts("End parent process");
+    return 0;    
+}
